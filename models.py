2db474a21710249d349d6cebe964fe9d061fbbd5
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-09 19:03:56 +1100

    :trollface: 2d is the new 3d

--- models.py
+++ models.py
@@ -162,17 +162,17 @@ class ResearchModels():
         """
         # Model.
         model = Sequential()
-        model.add(Conv3D(
+        model.add(Conv2D(
             32, (3,3,3), activation='relu', input_shape=self.input_shape
         ))
         model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-        model.add(Conv3D(64, (3,3,3), activation='relu'))
+        model.add(Conv2D(64, (3,3,3), activation='relu'))
         model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-        model.add(Conv3D(128, (3,3,3), activation='relu'))
-        model.add(Conv3D(128, (3,3,3), activation='relu'))
+        model.add(Conv2D(128, (3,3,3), activation='relu'))
+        model.add(Conv2D(128, (3,3,3), activation='relu'))
         model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
-        model.add(Conv3D(256, (2,2,2), activation='relu'))
-        model.add(Conv3D(256, (2,2,2), activation='relu'))
+        model.add(Conv2D(256, (2,2,2), activation='relu'))
+        model.add(Conv2D(256, (2,2,2), activation='relu'))
         model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
 
         model.add(Flatten())
