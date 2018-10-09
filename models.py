a37a82994f0e5ebe7c4473c5dcd82d5d082e2bb5
Sam Wemyss <samuel.wemyss@gmail.com>
2018-10-10 10:18:47 +1100

    change lrcn to be 1d time distrivuted

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from keras.layers.recurrent import LSTM
 from keras.models import Sequential, load_model
 from keras.optimizers import Adam, RMSprop
 from keras.layers.wrappers import TimeDistributed
-from keras.layers.convolutional import (Conv2D, MaxPooling3D, Conv3D,
+from keras.layers.convolutional import (Conv1D, MaxPooling1D, Conv2D, MaxPooling3D, Conv3D,
     MaxPooling2D)
 from collections import deque
 import sys
@@ -103,35 +103,35 @@ class ResearchModels():
         """
         model = Sequential()
 
-        model.add(TimeDistributed(Conv2D(32, (7, 7), strides=(2, 2),
+        model.add(TimeDistributed(Conv1D(32, 7, strides=2,
             activation='relu', padding='same'), input_shape=self.input_shape))
-        model.add(TimeDistributed(Conv2D(32, (3,3),
+        model.add(TimeDistributed(Conv1D(32, 3,
             kernel_initializer="he_normal", activation='relu')))
-        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        model.add(TimeDistributed(MaxPooling1D(2, strides=2)))
 
-        model.add(TimeDistributed(Conv2D(64, (3,3),
+        model.add(TimeDistributed(Conv1D(64, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(Conv2D(64, (3,3),
+        model.add(TimeDistributed(Conv1D(64, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        model.add(TimeDistributed(MaxPooling1D(2, strides=2)))
 
-        model.add(TimeDistributed(Conv2D(128, (3,3),
+        model.add(TimeDistributed(Conv1D(128, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(Conv2D(128, (3,3),
+        model.add(TimeDistributed(Conv1D(128, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        model.add(TimeDistributed(MaxPooling1D(2, strides=2)))
 
-        model.add(TimeDistributed(Conv2D(256, (3,3),
+        model.add(TimeDistributed(Conv1D(256, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(Conv2D(256, (3,3),
+        model.add(TimeDistributed(Conv1D(256, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        model.add(TimeDistributed(MaxPooling1D(2, strides=2)))
 
-        model.add(TimeDistributed(Conv2D(512, (3,3),
+        model.add(TimeDistributed(Conv1D(512, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(Conv2D(512, (3,3),
+        model.add(TimeDistributed(Conv1D(512, 3,
             padding='same', activation='relu')))
-        model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
+        model.add(TimeDistributed(MaxPooling1D(2, strides=2)))
 
         model.add(TimeDistributed(Flatten()))
 
